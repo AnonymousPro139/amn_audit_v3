0705f3641a0c65cdd3813a17223243ba7949006d
AnonymousPro139 <anonymouspro139@gmail.com>
2023-05-26 17:47:44 +0800

    'prod_version'

--- inc/conf.php
+++ inc/conf.php
@@ -1,9 +1,9 @@
 <?php
-const DOMAIN = "test.mn";
+const DOMAIN = "amnudit.mn";
 
 const DB_HOST = "127.0.0.1";
-const DB_NAME = "amn_test";
-const DB_USER = "root";
-const DB_PASSWORD = "";
+const DB_NAME = "amnaudit_prod_v1";
+const DB_USER = "amnaudit_coder125";
+const DB_PASSWORD = "coder125_+123";
 
 const SALT = '_amn';
