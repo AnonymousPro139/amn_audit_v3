822f12c82141db3ee4957bc5e42599ed4091db19
AnonymousPro139 <anonymouspro139@gmail.com>
2023-05-30 15:45:20 +0800

    dayansoft program gedgiig system bolgoj oorchlow

--- inc/conf.php
+++ inc/conf.php
@@ -1,9 +1,14 @@
 <?php
 const DOMAIN = "amnudit.mn";
 
+// const DB_HOST = "127.0.0.1";
+// const DB_NAME = "amnaudit_prod_v1";
+// const DB_USER = "amnaudit_coder125";
+// const DB_PASSWORD = "coder125_+123";
+
 const DB_HOST = "127.0.0.1";
-const DB_NAME = "amnaudit_prod_v1";
-const DB_USER = "amnaudit_coder125";
-const DB_PASSWORD = "coder125_+123";
+const DB_NAME = "amn_test";
+const DB_USER = "root";
+const DB_PASSWORD = "";
 
 const SALT = '_amn';
